1c0e6fec65ccbb3634bf9491bc3364a7ca74c6e0
david-irvinne <73836841+david-irvinne@users.noreply.github.com>
2024-10-30 20:00:20 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 #define int long long
 #define Int __int128_t
+#define bpc(x) __builtin_popcountll(x)
+
+#ifdef DEBUG
 #define dbg(x) cout<<"["<< #x <<"] : "<<(x)<<endl;
+#else
+#define dbg(x)
+#endif
 
 int bpow(int a, int b, long long mod=LLONG_MAX){
 	int res=1;while(b){if(b%2)res=res*a%mod;a=a*a%mod;b/=2;}return res;
